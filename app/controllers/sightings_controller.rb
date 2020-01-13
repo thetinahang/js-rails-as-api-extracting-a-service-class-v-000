765bc8dee0bbb335058274c6f011dc61668d4421
Tina Hang <mstinahang@gmail.com>
2020-01-13 15:12:14 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -6,7 +6,7 @@ class SightingsController < ApplicationController
       :location => {:only => [:latitude, :longitude]}
     }, :except => [:updated_at])
   end
-
+ 
   def show
     sighting = Sighting.find_by(id: params[:id])
     render json: sighting.to_json(:include => {
